96c19ff9cea20c8e1a8b6d8adbb277f7be18daa2
unknown <jpoloni321@gmail.com>
2026-08-03 22:12:43 -0300

    Projeto configurado

---
+++ src/main/java/com/jpoloni/funcionariosprojeto/FuncionariosprojetoApplication.java
@@ -0,0 +1,13 @@
+package com.jpoloni.funcionariosprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuncionariosprojetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuncionariosprojetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpoloni/funcionariosprojeto/FuncionariosprojetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpoloni.funcionariosprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncionariosprojetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
