cd92205b49b94f243a70d787ce3803e6b783b5f1
unknown <jpoloni321@gmail.com>
2026-08-10 22:12:47 -0300

    Acabamos o CRUD

---
+++ src/main/java/com/jpoloni/funcionariosprojeto/controller/FuncionarioController.java
@@ -0,0 +1,43 @@
+package com.jpoloni.funcionariosprojeto.controller;
+
+
+import com.jpoloni.funcionariosprojeto.models.FuncionarioModel;
+import com.jpoloni.funcionariosprojeto.services.FuncionarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/funcionarios")
+public class FuncionarioController {
+
+    @Autowired
+     private FuncionarioService funcionarioService;
+
+    @GetMapping
+    public List<FuncionarioModel> findAll(){
+        return funcionarioService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public FuncionarioModel buscarPorId(@PathVariable Long id){
+        return funcionarioService.buscarPorId(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar( @PathVariable Long id){
+        funcionarioService.deletarPessoa(id);
+    }
+
+    @PostMapping
+    public FuncionarioModel criarFuncionario( @RequestBody FuncionarioModel funcionarioModel){
+        return funcionarioService.criarPessoa(funcionarioModel);
+    }
+
+    public FuncionarioModel atualizar(@PathVariable Long id, @RequestBody FuncionarioModel funcionarioModel){
+        return funcionarioService.atualizar(id, funcionarioModel);
+    }
+
+
+}
